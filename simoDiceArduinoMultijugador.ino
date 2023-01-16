dda8093289e1871b9ef11bb31b68656e5c97b48a
Yasin BC <101365105+yasinbc@users.noreply.github.com>
2023-01-16 14:06:49 +0100

    Update simoDiceArduinoMultijugador.ino
    Limpiado codigo malo

--- simoDiceArduinoMultijugador.ino
+++ simoDiceArduinoMultijugador.ino
@@ -100,6 +100,8 @@ Adafruit_LiquidCrystal lcd_1(0);
 #define NOTA_D8  4699
 #define NOTA_DS8 4978
 
+#define BUZZER_PIN 11
+
 int color1 = 0;
 int color2 = 0;
 int color3 = 0;
@@ -109,29 +111,68 @@ int color6 = 0;
 int color7 = 0;
 int color8 = 0;
 
+int pines[3] = {5, 6, 7};
+int colores[3] = {4,3,2};
+int notasBuzzer[3] = {NOTA_E3, NOTA_A3, NOTA_D4};
+
 
 //Reproduce sonido ACERTAR cada vez que el jugador finaliza una secuencia con exito
 void reproduceSonidoAcierto() {
-    tone(11, NOTA_E4);
+    tone(BUZZER_PIN, NOTA_E4);
     delay(150);
-    tone(11, NOTA_G4);
+    tone(BUZZER_PIN, NOTA_G4);
     delay(150);
-    tone(11, NOTA_E5);
+    tone(BUZZER_PIN, NOTA_E5);
     delay(150);
-    tone(11, NOTA_C5);
+    tone(BUZZER_PIN, NOTA_C5);
     delay(150);
-    tone(11, NOTA_D5);
+    tone(BUZZER_PIN, NOTA_D5);
     delay(150);
-    tone(11, NOTA_G5);
+    tone(BUZZER_PIN, NOTA_G5);
     delay(150);
-    noTone(11);
+    noTone(BUZZER_PIN);
 }
+
+void gameOver() {
+    Serial.print("Game over!");
+    delay(200);
+
+    //Reproduce el sonido Wah-Wah-Wah-Wah cuando no aciertas
+    tone(BUZZER_PIN, NOTA_DS5);
+    delay(300);
+    tone(BUZZER_PIN, NOTA_D5);
+    delay(300);
+    tone(BUZZER_PIN, NOTA_CS5);
+    delay(300);
+    for (byte i = 0; i < 10; i++) {
+        for (int pitch = -10; pitch <= 10; pitch++) {
+        tone(1, NOTA_C5 + pitch);
+        delay(5);
+        }
+    }
+    noTone(BUZZER_PIN);
+    delay(500);
+}
+
+void leeBotones1(){
+  for(int i=0; i<=3; i++){
+    if (digitalRead(pines[i]) == HIGH && colores[i] != 0) {
+      colores[i]; //color1 = 4;
+      tone(BUZZER_PIN, notasBuzzer[i], 200);
+      digitalWrite(colores[i], HIGH);
+      delay(1000);
+      digitalWrite(colores[i], LOW);
+    }
+  }
+}
+
+
 
 void setup()
 {
   lcd_1.begin(16, 2);
   pinMode(5, INPUT);
-  pinMode(11, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(6, INPUT);
   pinMode(3, OUTPUT);
@@ -148,193 +189,193 @@ void loop()
 {
   if (digitalRead(5) == HIGH && color1 == 0) {
     color1 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200);
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000);
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(6) == HIGH && color1 == 0) {
     color1 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200);
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(7) == HIGH && color1 == 0) {
     color1 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
 
   if (digitalRead(5) == HIGH && color2 == 0) {
     color2 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200); 
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(6) == HIGH && color2 == 0) {
     color2 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200); 
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(7) == HIGH && color2 == 0) {
     color2 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200);
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
 
   if (digitalRead(5) == HIGH && color3 == 0) {
     color3 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200);
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(6) == HIGH && color3 == 0) {
     color3 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200); 
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(7) == HIGH && color3 == 0) {
     color3 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000);
     digitalWrite(2, LOW);
   }
   
   if (digitalRead(5) == HIGH && color4 == 0) {
     color4 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200); 
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(6) == HIGH && color4 == 0) {
     color4 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200);
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(7) == HIGH && color4 == 0) {
     color4 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
   
   if (digitalRead(8) == HIGH && color5 == 0) {
     color5 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200); 
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(9) == HIGH && color5 == 0) {
     color5 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200); 
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(10) == HIGH && color5 == 0) {
     color5 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
   
   if (digitalRead(8) == HIGH && color6 == 0) {
     color6 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200); 
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(9) == HIGH && color6 == 0) {
     color6 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200); 
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(10) == HIGH && color6 == 0) {
     color6 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
  
   if (digitalRead(8) == HIGH && color7 == 0) {
     color7 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200); 
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(9) == HIGH && color7 == 0) {
     color7 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200); 
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(10) == HIGH && color7 == 0) {
     color7 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
   
   if (digitalRead(8) == HIGH && color8 == 0) {
     color8 = 4;
-    tone(11, 165, 200); // tono 40 (E3 = 165 Hz)
+    tone(BUZZER_PIN, NOTA_E3, 200); 
     digitalWrite(4, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(4, LOW);
   }
 
   if (digitalRead(9) == HIGH && color8 == 0) {
     color8 = 3;
-    tone(11, 220, 200); // tono 45 (A3 = 220 Hz)
+    tone(BUZZER_PIN, NOTA_A3, 200); 
     digitalWrite(3, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(3, LOW);
   }
 
   if (digitalRead(10) == HIGH && color8 == 0) {
     color8 = 2;
-    tone(11, 294, 200); // tono 50 (D4 = 294 Hz)
+    tone(BUZZER_PIN, NOTA_D4, 200); 
     digitalWrite(2, HIGH);
-    delay(1000); // Esperar 1000 milisegundos
+    delay(1000); 
     digitalWrite(2, LOW);
   }
   
@@ -342,20 +383,20 @@ void loop()
 
   if (color1 != 0 && (color2 != 0 && (color3 != 0 && (color4 != 0 && (color5 != 0 && (color6 != 0 && (color7 != 0 && color8 != 0))))))) {
     if (color1 == color5 && (color2 == color6 && (color3 == color7 && color4 == color8))) {
-      tone(11, 1661, 200); // tono 80 (G#6 = 1661 Hz)
+      tone(BUZZER_PIN, NOTA_F5, 200); 
       digitalWrite(4, HIGH);
       delay(200); 
-      tone(11, 2217, 200); // tono 85 (C#7 = 2217 Hz)
+      tone(BUZZER_PIN, NOTA_G5, 200); 
       digitalWrite(3, HIGH);
       delay(200); 
-      tone(11, 2960, 500); // tono 90 (F#7 = 2960 Hz)
+      tone(BUZZER_PIN, NOTA_A5, 600);
       digitalWrite(2, HIGH);
       delay(500); 
       digitalWrite(2, LOW);
       delay(200); 
       digitalWrite(3, LOW);
       delay(200); 
-      digitalWrite(4, LOW);
+      digitalWrite(4, LOW);	
 
       
       Serial.println("Sucuencia bien hecha!!");
@@ -378,9 +419,10 @@ void loop()
       lcd_1.setCursor(0, 0);
     } else {
 
-      tone(11, 92, 200); // tono 30 (F#2 = 92 Hz)
+      tone(11, 92, 200); 
       Serial.println("Secuencia mal hecha!");
       Serial.println("Empieza de nuevo!");
+      gameOver();
       lcd_1.print("MAL!");
 
       //Resetea las variables para la siguiente partida
